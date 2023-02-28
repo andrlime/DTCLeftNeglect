c1b4aaf498fa79a67e0250e16980bc795c0c1974
Andrew Li <54134293+andrlime@users.noreply.github.com>
2023-02-27 20:26:38 -0600

    remove broken line of code

--- LeftNeglectRN/src/components/DropdownCallback.tsx
+++ LeftNeglectRN/src/components/DropdownCallback.tsx
@@ -30,7 +30,7 @@ export const DropdownCallback: React.FC<Props> = ({ label, options, value, callb
                 >
                     <Picker.Item label={"Press to select..."} value={"S"} />
                     {options.map((e) => (
-                        <Picker.Item key={e.label+e.value+Math.random(1)} label={e.label} value={e.value} />
+                        <Picker.Item key={e.label+e.value+Math.random()} label={e.label} value={e.value} />
                     ))}
                 </Picker>
                 <Button title='Done' onPress={() => setShow(false)}/>
