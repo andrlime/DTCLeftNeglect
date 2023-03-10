dfa16396098cdda75469db38d49c20eb52c87080
Andrew Li <54134293+andrlime@users.noreply.github.com>
2023-03-10 15:06:33 -0600

    proof of concept

--- LeftNeglectPythonAPI/run.py
+++ LeftNeglectPythonAPI/run.py
@@ -31,7 +31,7 @@ class MPUData(LittleEndianStructure):
                 ("gyroZ", c_float),
                 ("time", c_ulong)]
 
-async def read() -> MPUData:
+async def connect():
     print("**SCANNING")
 
     devices = await BleakScanner.discover(
@@ -46,14 +46,48 @@ async def read() -> MPUData:
         print("**TOO MANY DEVICES")
         return
     
+    # for device in devices:
+    #     print device
+    
     async with BleakClient(
         list(devices.keys())[0],
         services=[_SERVICE_UUID]) as client:
         print("connected!")
+        return client
 
-        gatt = (await client.read_gatt_char(_DATA_CHAR))
-        data = MPUData.from_buffer(gatt)
-        return data
+async def read(client: BleakClient) -> MPUData:
+    # Read from device and parse it as MPUData
+    gatt = (await client.read_gatt_char(_DATA_CHAR))
+    data = MPUData.from_buffer(gatt)
+    print(data)
+
+    # Handle data here
+    # Also note that calibration would involve another number --> that number can be subtracted/added as necessary
+    print(f"Normalized number of degrees = {data.roll/360}")
+
+    return data
+
+# Write token to device UUID
+async def write(token, client: BleakClient) -> bool:
+    # Write gatt char to device
+    await client.write_gatt_char(char_specifier = _DATA_CHAR, data = token)
+    print(f"wrote {token} to device")
+    return True
+    
+# Set buzzer UUID to 1, and then 0
+async def buzz():
+    await write(b'1', _BUZZ_UUID) # buzz
+    await write(b'0', _BUZZ_UUID) # stop buzzing
+
+async def main() :
+    client = await connect()
+    if not client:
+        return 
+    
+    client.mtu_size = 517
+
+    await read(client)
+    await write(b'1', client)
 
 if __name__ == "__main__":
-    asyncio.run(read())
+    asyncio.run(main())
